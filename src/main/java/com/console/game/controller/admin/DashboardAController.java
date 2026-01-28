35cc5926e25fef536e47141c0288a55790b671bf
nganbb2 <vttngan2022@gmial.com>
2026-01-28 09:02:21 +0700

    Dang lam phan admin

---
+++ src/main/java/com/console/game/controller/admin/DashboardAController.java
@@ -0,0 +1,12 @@
+package com.console.game.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardAController {
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin/dashboard";
+    }
+}
